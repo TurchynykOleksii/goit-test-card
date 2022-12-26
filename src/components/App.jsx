325a79cd0f796eaf99b375e6b3ee152bdc1f1894
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2022-12-26 13:49:49 +0200

    done test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Card } from './Card';
+import dataUser from '../data/data.json';
+
+export class App extends Component {
+  state = {
+    data: dataUser[0],
+    isFollow: false,
+  };
+
+  componentDidMount() {
+    const localStorageUser = JSON.parse(localStorage.getItem('user'));
+    if (localStorageUser) {
+      this.setState({
+        data: localStorageUser,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { data } = this.state;
+    if (data !== prevState.data) {
+      localStorage.setItem('user', JSON.stringify(data));
+    }
+  }
+
+  handleBtn = () => {
+    const newObj = { ...this.state.data };
+    newObj.isFollow = !this.state.isFollow;
+
+    this.setState({
+      data: newObj,
+      isFollow: !this.state.isFollow,
+    });
+  };
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <>
+        <Card handleButton={this.handleBtn} stateInfo={this.state} />
+      </>
+    );
+  }
+}
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,44 @@
+import css from './Card.module.css';
+import bg from '../../background.png';
+import logo from '../../logo.png';
+
+export const Card = ({ handleButton, stateInfo }) => {
+  const {
+    data: { avatar, user, followers, tweets, isFollow },
+  } = stateInfo;
+  console.log(isFollow);
+  let updateFollower = isFollow ? followers + 1 : followers;
+  return (
+    <div className={css.card}>
+      <div className={css.logo}>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="asdasda">
+        <img src={bg} alt="background" />
+      </div>
+      <div className={css.userInfoContainer}>
+        <div className={css.avatarContainer}>
+          <img className={css.avatar} src={avatar} alt="" />
+        </div>
+        <p className={css.user}>{user}</p>
+        <p className={css.tweets}>
+          <span className={css.tweets}>{tweets}</span>
+          <span>tweets</span>
+        </p>
+        <p className={css.followers}>
+          <span className={css.followers}>
+            {updateFollower.toLocaleString('EN-en')}
+          </span>
+          <span>Followers</span>
+        </p>
+        <button
+          className={`${css.button} + ${isFollow && css.active}`}
+          type="button"
+          onClick={() => handleButton()}
+        >
+          {isFollow ? 'Following' : 'Follow'}
+        </button>
+      </div>
+    </div>
+  );
+};
